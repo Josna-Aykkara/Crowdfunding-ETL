5e2b6a7a847b9b56cc2e1800706dafc2154e45a7
unknown <josna.aykkara@gmail.com>
2022-10-19 22:08:09 -0400

    Module Challenge

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Crowdfunding-ETL
+
+## Overview
+
+The scope of the project is to extract, transform and load the information from the database. ERD is prepared to identify the elements in the table and to make a note of the primary key and foreign keys in the table.
+It is used to connect the data between the various tables, as we have to combine information’s from different tables to analysis this project.
+
+## Results
+The crowdfunding project was used to get the information regarding the backers for the projects and to throw light on the remaining goal amounts to be achieved. It facilitates to concentrate on the backers for the remaining goal at a glance. It provides access to the required information like the contact information and personal information such as first name and last name. As a result the ETL enables to make the presentation of information for concise, accurate and complete as it was gathered from multiple tables.  
---
+++ crowdfunding_SQL_Analysis.sql
@@ -0,0 +1,61 @@
+-- Challenge Bonus queries.
+-- 1. (2.5 pts)
+-- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
+SELECT cf_id, backers_count
+FROM campaign
+WHERE outcome = 'live'
+ORDER BY backers_count DESC;
+
+
+-- 2. (2.5 pts)
+-- Using the "backers" table confirm the results in the first query.
+SELECT cf_id, COUNT(backer_id) AS backers_count
+FROM backers
+GROUP B cf_id
+ORDER BY backers_count DESC;
+
+
+-- 3. (5 pts)
+-- Create a table that has the first and last name, and email address of each contact.
+-- and the amount left to reach the goal for all "live" projects in descending order. 
+SELECT C.first_name, C.last_name,C.email, (CA.goal-CA.pledged) AS "Remaining Goal Amount"
+INTO email_contacts_remaining_goal_amount
+FROM contacts AS C 
+JOIN campaign AS CA
+ON CA.contact_id = C.contact_id
+WHERE CA.outcome = 'live'
+ORDER BY "Remaining Goal Amount" DESC;
+
+
+
+-- Check the table
+SELECT * FROM email_contacts_remaining_goal_amount
+
+-- 4. (5 pts)
+-- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
+-- and has the first and last name of each backer, the cf_id, company name, description, 
+-- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
+SELECT B.email, B.first_name, B.last_name, CA.cf_id, CA.company_name, CA.description, CA.end_date, (CA.goal-CA.pledged) AS "Left of Goal" 
+INTO email_backers_remaining_goal_amount
+FROM backers AS B  
+JOIN campaign AS CA
+ON CA.cf_id = B.cf_id
+WHERE CA.outcome = 'live'
+ORDER BY B.email DESC;
+
+
+-- Check the table
+SELECT * FROM email_backers_remaining_goal_amount
+
+-- To get teh result matching teh image in the Material book
+SELECT B.email, B.first_name, B.last_name, CA.cf_id, CA.company_name, CA.description, CA.end_date, (CA.goal-CA.pledged) AS "Left of Goal" 
+INTO email_backers_remaining_goal_amount
+FROM backers AS B  
+JOIN campaign AS CA
+ON CA.cf_id = B.cf_id
+WHERE CA.outcome = 'live'
+ORDER BY B.last_name, B.first_name ASC;
+-- AESC;
+
+-- Check the table
+SELECT * FROM email_backers_remaining_goal_amount
